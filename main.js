90de6a80fb0e2e87f60f37ca89802c4c4c690c54
JuanC <jbolattitorcasio@frba.utn.edu.ar>
2023-11-15 19:03:36 -0300

    nuevos creditos, imagenes y to top

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ function scrollToElement(id) {
     var cancion = document.getElementById(id);
     scrollTo(cancion);
 }
+
+window.onscroll = () => {
+    if(window.scrollY > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+let mybutton = document.getElementById("myBtn");
+
+function topFunction() {
+  document.body.scroll({
+    top: 0,
+    behavior: "smooth"
+  });
+  document.documentElement.scroll({
+    top: 0,
+    behavior: "smooth"
+  });
+}
